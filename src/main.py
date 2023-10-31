9779a6d67c2d108cc2555dbdd4884894aa722fbe
locket0115 <max0115.kang@gmail.com>
2023-11-01 01:13:20 +0900

    config

--- src/main.py
+++ src/main.py
@@ -24,8 +24,10 @@ class Main:
         while True:
             # show methods
             game.show_bg(screen)
+            game.show_last_move(screen)
             game.show_moves(screen)
             game.show_pieces(screen)
+            game.show_hover(screen)
 
             if dragger.dragging:
                 dragger.update_blit(screen)
@@ -42,22 +44,32 @@ class Main:
                     # if clicked square has a piece ?
                     if board.squares[clicked_row][clicked_col].has_piece():
                         piece = board.squares[clicked_row][clicked_col].piece
-                        board.calc_moves(piece, clicked_row, clicked_col)
-                        dragger.save_initial(event.pos)
-                        dragger.drag_piece(piece)
-                        # show methods
-                        game.show_bg(screen)
-                        game.show_moves(screen)
-                        game.show_pieces(screen)
+                        # vaild piece (color) ?
+                        if piece.color == game.next_player:
+                            board.calc_moves(piece, clicked_row, clicked_col)
+                            dragger.save_initial(event.pos)
+                            dragger.drag_piece(piece)
+                            # show methods
+                            game.show_bg(screen)
+                            game.show_last_move(screen)
+                            game.show_moves(screen)
+                            game.show_pieces(screen)
 
                 # mouse motion
                 elif event.type == pygame.MOUSEMOTION:
+                    motion_row = event.pos[1] // SQSIZE
+                    motion_col = event.pos[0] // SQSIZE
+                    
+                    game.set_hover(motion_row, motion_col)
+
                     if dragger.dragging:
                         dragger.update_mouse(event.pos)
                         # show methods
                         game.show_bg(screen)
+                        game.show_last_move(screen)
                         game.show_moves(screen)
                         game.show_pieces(screen)
+                        game.show_hover(screen)
                         dragger.update_blit(screen)
 
                 #click release
@@ -70,19 +82,32 @@ class Main:
                         released_col = dragger.mouseX // SQSIZE
 
                         # create possible move
-
                         initial = Square(dragger.initial_row, dragger.initial_col)
                         final = Square(released_row, released_col)
                         move = Move(initial, final)
 
                         # vaild move ?
                         if board.vaild_move(dragger.piece, move):
+                            captured = board.squares[released_row][released_col].has_piece()
+
                             board.move(dragger.piece, move)
+                            # sounds
+                            game.play_sound(captured)
                             # show methods
                             game.show_bg(screen)
+                            game.show_last_move(screen)
                             game.show_pieces(screen)
+                            # next turn
+                            game.next_turn()
 
                     dragger.undrag_piece()
+
+                # key press
+                elif event.type == pygame.KEYDOWN:
+
+                    # changing theme
+                    if event.key == pygame.K_t:
+                        game.change_theme()
 
                 #quit application
                 if event.type == pygame.QUIT:
